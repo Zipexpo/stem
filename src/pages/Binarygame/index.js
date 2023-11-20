f6bd04261aa5bbefb709598ed5cd5f52507302f5
nvtngan <nvtngan@hcmus.edu.vn>
2023-11-20 08:30:59 -0600

    update

--- src/pages/Binarygame/index.js
+++ src/pages/Binarygame/index.js
@@ -1,8 +1,9 @@
 
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState, useMemo } from 'react'
 import { create } from 'zustand'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { Text, Environment } from '@react-three/drei'
+import {Text, Environment, Extrude, Grid} from '@react-three/drei'
+import {Shape} from "three";
 import Fireflies from '../../components/3Dcoponents/Fireflies'
 
 const useGameStore = create((set) => ({
@@ -14,10 +15,16 @@ const useGameStore = create((set) => ({
     resetScore: () => set({ score: 0 }),
 }))
 
-export default function() {
+export default function({circleRadius=0.5}) {
     const { timer, decreaseTime, resetTimer, score, increaseScore, resetScore } = useGameStore()
     const timerRef = useRef();
     const [currentState,setcurrentState] = useState(10);
+    const roundBoxShape=useMemo(()=>new Shape()
+        .moveTo( 0, circleRadius )
+        .quadraticCurveTo( circleRadius, circleRadius, circleRadius, 0 )
+        .quadraticCurveTo( circleRadius, - circleRadius, 0, - circleRadius )
+        .quadraticCurveTo( - circleRadius, - circleRadius, - circleRadius, 0 )
+        .quadraticCurveTo( - circleRadius, circleRadius, 0, circleRadius ),[circleRadius])
     useEffect(()=>{
         timerRef.current = setInterval(() => {
             decreaseTime();
@@ -28,15 +35,31 @@ export default function() {
         if (timer<=0)
             clearInterval(timerRef.current);
     },[timer]);
-    return <div className="container relative mx-auto p-4 w-full" style={{height:'400px'}}>
-        <Canvas>
+    return <div className="container relative mx-auto p-4 w-full" style={{height:'600px'}}>
+        <Canvas
+            raycaster={{params: {Points: {threshold: 0.01}}}}
+            camera={{position: [0, 15, 9], fov: 25}}
+        >
             <ambientLight intensity={0.5} />
             <Environment files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/potsdamer_platz_1k.hdr" />
             <Fireflies count={20} radius={80} colors={['orange']} />
-
-            <Text color="black" anchorX="center" anchorY="middle">
-                {timer}
-            </Text>
+            <group rotation={[-Math.PI/2,0,0]}>
+                <Extrude args={[roundBoxShape, {depth:0.1,bevelEnabled: false}]}>
+                    <meshStandardMaterial color={'#797979'} />
+                </Extrude>
+            </group>
+            <Grid position={[0, -0.02, 0]} args={[100,100]} {...{
+                cellSize: 5 / 5 / 5,
+                cellThickness: 1,
+                cellColor: '#6f6f6f',
+                sectionSize: 5 / 5,
+                sectionThickness: 1.5,
+                sectionColor: '#9d4b4b',
+                fadeDistance: 25,
+                fadeStrength: 1,
+                followCamera: true,
+                infiniteGrid: true
+            }}/>
         </Canvas>
         <span className="text-4xl text-cyan-900 font-bold absolute top-0 left-0 m-10">{timer}</span>
     </div>
